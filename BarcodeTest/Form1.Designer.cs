b01070fc6aa9c23daf7a78fdc4ec4ab5c0476ae3
simonecosci <simone.cosci@gmail.com>
2018-10-19 18:20:58 +0200

    First commit

---
+++ BarcodeTest/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace BarcodeTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.code = new System.Windows.Forms.TextBox();
+            this.format = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.width = new System.Windows.Forms.NumericUpDown();
+            this.height = new System.Windows.Forms.NumericUpDown();
+            this.margin = new System.Windows.Forms.NumericUpDown();
+            this.picture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.width)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.height)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.margin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // code
+            // 
+            this.code.Location = new System.Drawing.Point(12, 98);
+            this.code.Name = "code";
+            this.code.Size = new System.Drawing.Size(378, 20);
+            this.code.TabIndex = 0;
+            this.code.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // format
+            // 
+            this.format.FormattingEnabled = true;
+            this.format.Items.AddRange(new object[] {
+            "EAN_13",
+            "UPC_A"});
+            this.format.Location = new System.Drawing.Point(75, 8);
+            this.format.Name = "format";
+            this.format.Size = new System.Drawing.Size(121, 21);
+            this.format.TabIndex = 1;
+            this.format.SelectedIndexChanged += new System.EventHandler(this.format_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Format";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "width";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "height";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(158, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "margin";
+            // 
+            // width
+            // 
+            this.width.Location = new System.Drawing.Point(12, 61);
+            this.width.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.width.Name = "width";
+            this.width.Size = new System.Drawing.Size(50, 20);
+            this.width.TabIndex = 6;
+            this.width.Value = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
+            // 
+            // height
+            // 
+            this.height.Location = new System.Drawing.Point(89, 60);
+            this.height.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.height.Name = "height";
+            this.height.Size = new System.Drawing.Size(47, 20);
+            this.height.TabIndex = 7;
+            this.height.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // margin
+            // 
+            this.margin.Location = new System.Drawing.Point(161, 61);
+            this.margin.Name = "margin";
+            this.margin.Size = new System.Drawing.Size(49, 20);
+            this.margin.TabIndex = 8;
+            // 
+            // picture
+            // 
+            this.picture.Location = new System.Drawing.Point(13, 156);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(377, 89);
+            this.picture.TabIndex = 9;
+            this.picture.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 385);
+            this.Controls.Add(this.picture);
+            this.Controls.Add(this.margin);
+            this.Controls.Add(this.height);
+            this.Controls.Add(this.width);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.format);
+            this.Controls.Add(this.code);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.width)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.height)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.margin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox code;
+        private System.Windows.Forms.ComboBox format;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown width;
+        private System.Windows.Forms.NumericUpDown height;
+        private System.Windows.Forms.NumericUpDown margin;
+        private System.Windows.Forms.PictureBox picture;
+    }
+}
+
---
+++ BarcodeTest/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BarcodeTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            format.Text = "EAN_13";
+            code.Focus();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            string barcode = code.Text;
+
+            //System.Diagnostics.Debug.WriteLine(barcode + " -> " + barcode.Length.ToString());
+            if (format.Text == "EAN_13" && barcode.Length == 13)
+            {
+                process(barcode);
+            }
+
+            if (format.Text == "UPC_A" && barcode.Length == 12)
+            {
+                process(barcode);
+            }
+
+        }
+
+
+        protected void process(string code)
+        {
+            MessageBox.Show(code);
+            var brcode = new ZXing.BarcodeWriter();
+            var encOptions = new ZXing.Common.EncodingOptions() { Width = Convert.ToInt32(width.Value), Height = Convert.ToInt32(height.Value), Margin = Convert.ToInt32(margin.Value) };
+            brcode.Options = encOptions;
+            switch (format.Text)
+            {
+                case "EAN_13":
+                    brcode.Format = ZXing.BarcodeFormat.EAN_13;
+                    break;
+                case "UPC_A":
+                    brcode.Format = ZXing.BarcodeFormat.UPC_A;
+                    break;
+                default:
+                    MessageBox.Show("Unsupported");
+                    return;
+            }
+            Bitmap result = new Bitmap(brcode.Write(code));
+            result.Save("test.bmp");
+            picture.ImageLocation = "test.bmp";
+        }
+
+        private void format_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            code.Focus();
+        }
+    }
+}
